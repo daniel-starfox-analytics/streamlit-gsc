a612e01c5ed4847081a73b5901dc0e86c8d10829
dtaubaso <d.taubaso@gmail.com>
2024-12-31 23:03:39 -0300

    agreega para mostrar datos

--- app.py
+++ app.py
@@ -93,7 +93,11 @@ def main():
         properties = list_gsc_properties(st.session_state.credentials)
 
         if properties:
-            st.write(properties)
+            selected_property = st.selectbox("Seleccione una propiedad", properties)
+            webproperty = account[selected_property]
+            with st.button("Obtener clicks"):
+                df = webproperty.query.range('today',days=-7).dimension('date').get().to_dataframe()
+                st.write(df)
         else:
             st.write("No hay nada aquí")
 
