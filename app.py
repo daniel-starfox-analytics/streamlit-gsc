5bd1726e1dfa729857124b5466bf8e939660d2fd
dtaubaso <d.taubaso@gmail.com>
2025-01-05 19:45:42 -0300

    agrega funciones del original de lee foot

--- app.py
+++ app.py
@@ -2,6 +2,65 @@ import streamlit as st
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 import searchconsole
+import datetime, time
+import pandas as pd
+
+# TODO: Seleccionar dispositivo
+
+
+# Constants
+SEARCH_TYPES = ["web", "image", "video", "news", "discover", "googleNews"]
+DATE_RANGE_OPTIONS = [
+    'Últimos 7 días',
+    'Últimos 30  días',
+    'Últimos 3 meses',
+    'Últimos 6 meses',
+    'Últimos 12 meses',
+    'Últimos 16 meses',
+    'Elegir fechas'
+]
+DEVICE_OPTIONS = ["Todos", "desktop", "mobile", "tablet"]
+BASE_DIMENSIONS = ["page", "query", "country", "date"]
+MAX_ROWS = 1_000_000
+DF_PREVIEW_ROWS = 100
+
+def setup_streamlit():
+    """
+    Configures Streamlit's page settings and displays the app title and markdown information.
+    Sets the page layout, title, and markdown content with links and app description.
+    """
+    st.set_page_config(page_title="Simple GSC Api Connector")
+    st.title("Simple GSC Api Connector")
+    url = "https://github.com/searchsolved/search-solved-public-seo/tree/main/search-console/streamlit-simple-gsc-connector"
+    st.write(f"[Código original por Lee Foot](url)")
+    st.divider()
+
+
+def init_session_state():
+    """
+    Initialises or updates the Streamlit session state variables for property selection,
+    search type, date range, dimensions, and device type.
+    """
+    if 'selected_property' not in st.session_state:
+        st.session_state.selected_property = None
+    if 'selected_search_type' not in st.session_state:
+        st.session_state.selected_search_type = 'web'
+    if 'selected_date_range' not in st.session_state:
+        st.session_state.selected_date_range = 'Últimos 7 días'
+    if 'start_date' not in st.session_state:
+        st.session_state.start_date = datetime.date.today() - datetime.timedelta(days=7)
+    if 'end_date' not in st.session_state:
+        st.session_state.end_date = datetime.date.today()
+    if 'selected_dimensions' not in st.session_state:
+        st.session_state.selected_dimensions = ['page', 'query']
+    if 'selected_device' not in st.session_state:
+        st.session_state.selected_device = 'Todos'
+    if 'custom_start_date' not in st.session_state:
+        st.session_state.custom_start_date = datetime.date.today() - datetime.timedelta(days=7)
+    if 'custom_end_date' not in st.session_state:
+        st.session_state.custom_end_date = datetime.date.today()
+
+
 
 def load_config():
     """
@@ -66,14 +125,191 @@ def list_gsc_properties(credentials):
     """
     service = build('webmasters', 'v3', credentials=credentials)
     site_list = service.sites().list().execute()
-    return [site['siteUrl'] for site in site_list.get('siteEntry', [])] or ["No properties found"]
+    return [site['siteUrl'] for site in site_list.get('siteEntry', [])] or ["No se encontraron propiedades"]
+
+
+def fetch_gsc_data(webproperty, search_type, start_date, end_date, dimensions, device_type=None):
+    """
+    Fetches Google Search Console data for a specified property, date range, dimensions, and device type.
+    Handles errors and returns the data as a DataFrame.
+    """
+    query = webproperty.query.range(start_date, end_date).search_type(search_type).dimension(*dimensions)
+
+    if 'device' in dimensions and device_type and device_type != 'Todos':
+        query = query.filter('device', 'equals', device_type.lower())
+
+    try:
+        return query.limit(MAX_ROWS).get().to_dataframe()
+    except Exception as e:
+        show_error(e)
+        return pd.DataFrame()
+
+
+def fetch_data_loading(webproperty, search_type, start_date, end_date, dimensions, device_type=None):
+    """
+    Fetches Google Search Console data with a loading indicator. Utilises 'fetch_gsc_data' for data retrieval.
+    Returns the fetched data as a DataFrame.
+    """
+    with st.spinner('Extrayendo data...'):
+        return fetch_gsc_data(webproperty, search_type, start_date, end_date, dimensions, device_type)
+
+
+# -------------
+# Utility Functions
+# -------------
+
+def update_dimensions(selected_search_type):
+    """
+    Updates and returns the list of dimensions based on the selected search type.
+    Adds 'device' to dimensions if the search type requires it.
+    """
+    return BASE_DIMENSIONS + ['device'] if selected_search_type != 'discover' else BASE_DIMENSIONS
+
+
+def calc_date_range(selection, custom_start=None, custom_end=None):
+    """
+    Calculates the date range based on the selected range option.
+    Returns the start and end dates for the specified range.
+    """
+    range_map = {
+        'Últimos 7 días': 7,
+        'Últimos 30  días': 30,
+        'Últimos 3 meses': 90,
+        'Últimos 6 meses': 180,
+        'Últimos 12 meses': 365,
+        'Últimos 16 meses': 480
+    }
+    today = datetime.date.today()
+    if selection == 'Elegir fechas':
+        if custom_start and custom_end:
+            return custom_start, custom_end
+        else:
+            return today - datetime.timedelta(days=7), today
+    return today - datetime.timedelta(days=range_map.get(selection, 0)), today
+
+
+def show_error(e):
+    """
+    Displays an error message in the Streamlit app.
+    Formats and shows the provided error 'e'.
+    """
+    st.error(f"Ocurrió un error: {e}")
+
+
+def property_change():
+    """
+    Updates the 'selected_property' in the Streamlit session state.
+    Triggered on change of the property selection.
+    """
+    st.session_state.selected_property = st.session_state['selected_property_selector']
+
+def show_property_selector(properties, account):
+    """
+    Displays a dropdown selector for Google Search Console properties.
+    Returns the selected property's webproperty object.
+    """
+    selected_property = st.selectbox(
+        "Seleccione una propiedad de Search Console:",
+        properties,
+        index=properties.index(
+            st.session_state.selected_property) if st.session_state.selected_property in properties else 0,
+        key='selected_property_selector',
+        on_change=property_change
+    )
+    return account[selected_property]
+
+def show_search_type_selector():
+    """
+    Displays a dropdown selector for choosing the search type.
+    Returns the selected search type.
+    """
+    return st.selectbox(
+        "Seleccione tipo de búsqueda:",
+        SEARCH_TYPES,
+        index=SEARCH_TYPES.index(st.session_state.selected_search_type),
+        key='search_type_selector'
+    )
+
+
+def show_date_range_selector():
+    """
+    Displays a dropdown selector for choosing the date range.
+    Returns the selected date range option.
+    """
+    return st.selectbox(
+        "Seleccione el rango de fechas:",
+        DATE_RANGE_OPTIONS,
+        index=DATE_RANGE_OPTIONS.index(st.session_state.selected_date_range),
+        key='date_range_selector'
+    )
+
+
+def show_custom_date_inputs():
+    """
+    Displays date input fields for custom date range selection.
+    Updates session state with the selected dates.
+    """
+    st.session_state.custom_start_date = st.date_input("Fecha inicio", st.session_state.custom_start_date)
+    st.session_state.custom_end_date = st.date_input("Fecha fin", st.session_state.custom_end_date)
+
+
+def show_dimensions_selector(search_type):
+    """
+    Displays a multi-select box for choosing dimensions based on the selected search type.
+    Returns the selected dimensions.
+    """
+    available_dimensions = update_dimensions(search_type)
+    return st.multiselect(
+        "Seleccione Dimensiones:",
+        available_dimensions,
+        default=st.session_state.selected_dimensions,
+        key='dimensions_selector'
+    )
+
+
+def show_fetch_data_button(webproperty, search_type, start_date, end_date, selected_dimensions):
+    """
+    Displays a button to fetch data based on selected parameters.
+    Shows the report DataFrame and download link upon successful data fetching.
+    """
+    if st.button("Extraer Data"):
+        report = fetch_data_loading(webproperty, search_type, start_date, end_date, selected_dimensions)
+
+        if report is not None:
+            show_dataframe(report)
+            download_csv(report)
+
+
+# -------------
+# File & Download Operations
+# -------------
+
+def show_dataframe(report):
+    """
+    Shows a preview of the first 100 rows of the report DataFrame in an expandable section.
+    """
+    with st.expander("Preview the First 100 Rows"):
+        st.dataframe(report.head(DF_PREVIEW_ROWS))
+
+
+def download_csv(report):
+    """
+    Generates and displays a download link for the report DataFrame in CSV format.
+    """
+    df = report.to_csv(index=False).encode('utf-8')
+    st.download_button(
+            label="Descargar como CSV",
+            data=df,
+            file_name=f'gsc_report_{int(time.time())}.csv',
+            mime='text/csv'
+        )
 
 def main():
     """
     The main function for the Streamlit application.
     Handles the app setup, authentication, UI components, and data fetching logic.
     """
-    
+    setup_streamlit()
     client_config = load_config()
     st.session_state.auth_flow, st.session_state.auth_url = google_auth(client_config)
     auth_code = None
@@ -84,22 +320,28 @@ def main():
         st.session_state.credentials = st.session_state.auth_flow.credentials
 
     if not st.session_state.get('credentials'):
-        if st.button("Sign in with Google"):
+        if st.button("Autentificarse con Google"):
             # Open the authentication URL
-            st.write('Please click the link below to sign in:')
+            st.write('Ingrese al siguiente link:')
+            st.write('No se guardarán sus datos')
             st.markdown(f'[Google Sign-In]({st.session_state.auth_url})', unsafe_allow_html=True)
     else:
+        init_session_state()
         account = auth_search_console(client_config, st.session_state.credentials)
         properties = list_gsc_properties(st.session_state.credentials)
 
         if properties:
-            selected_property = st.selectbox("Seleccione una propiedad", properties)
-            webproperty = account[selected_property]
-            if st.button("Obtener datos"):
-                df = webproperty.query.range('today',days=-7).dimension('page').get().to_dataframe()
-                st.write(df)
-        else:
-            st.write("No hay nada aquí")
+            webproperty = show_property_selector(properties, account)
+            search_type = show_search_type_selector()
+            date_range_selection = show_date_range_selector()
+            if date_range_selection == 'Elegir fechas':
+                show_custom_date_inputs()
+                start_date, end_date = st.session_state.custom_start_date, st.session_state.custom_end_date
+            else:
+                start_date, end_date = calc_date_range(date_range_selection)
+
+            selected_dimensions = show_dimensions_selector(search_type)
+            show_fetch_data_button(webproperty, search_type, start_date, end_date, selected_dimensions)
 
 
 if __name__ == "__main__":
